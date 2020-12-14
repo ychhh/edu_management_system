d22a72c19d56d1ea0a1377a12b5cb1145232a908
yuchenghang <yuchenghang_i@didichuxing.com>
2020-12-14 11:44:27 +0800

    二次提交

--- java/com/ychhh/edu_management_system/EduManagementSystemApplication.java
+++ java/com/ychhh/edu_management_system/EduManagementSystemApplication.java
@@ -1,11 +1,7 @@
 package com.ychhh.edu_management_system;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
 
 @SpringBootApplication
 public class EduManagementSystemApplication {
--- java/com/ychhh/edu_management_system/service/FileService.java
+++
@@ -1,7 +0,0 @@
-package com.ychhh.edu_management_system.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileService {
-    public boolean upload(MultipartFile file);
-}
